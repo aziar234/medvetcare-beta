f98c2c0af721187fd2b966266e3cb915f1a9fd63
aziar234 <154688433+aziar234@users.noreply.github.com>
2024-07-30 16:57:48 +0200

    Delete MedVetCare/fr/MedVetCare directory

--- MedVetCare/fr/MedVetCare/src/javascript/menu.js
+++
@@ -1,8 +0,0 @@
-const menuButton = document.getElementById('menu-button');
-const dropdownMenu = document.getElementById('dropdown-menu');
-
-
-menuButton.addEventListener('click', () => {
-  dropdownMenu.style.display = dropdownMenu.style.display === 'block'? 'none' : 'block';
-  
-});
